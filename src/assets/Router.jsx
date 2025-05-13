f7b2f417ea25d8b7156e8daa8bb7351399c25096
Ahsanul Haque Nehal <007cryptic@gmail.com>
2025-05-13 13:53:34 +0600

    initial commit

---
+++ src/assets/Router.jsx
@@ -0,0 +1,11 @@
+import Nav from "./components/Nav";
+
+export default function Router() {
+  return (
+    <>
+      <div className="w-[960px] mx-auto">
+        <Nav />
+      </div>
+    </>
+  );
+}
---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <>
+      <div className="h-[120px] flex justify-between items-center ">
+        <h1 className="text-4xl font-bold text-gray-800">Simple Blog</h1>
+        <img className="w-[50px]" src="nav-pic.png" alt="Nav Pic" />
+      </div>
+      <hr className="text-gray-300"/>
+    </>
+  );
+}
